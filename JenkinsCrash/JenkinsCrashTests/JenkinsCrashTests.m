c5b88f7a37678dd7aae1284b394f9bacc2cff2c5
USER <894143788@qq.com>
2016-08-21 14:21:08 +0800

    comment
    dddd

---
+++ JenkinsCrash/JenkinsCrashTests/JenkinsCrashTests.m
@@ -0,0 +1,39 @@
+//
+//  JenkinsCrashTests.m
+//  JenkinsCrashTests
+//
+//  Created by USER on 16/8/21.
+//  Copyright © 2016年 USER. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JenkinsCrashTests : XCTestCase
+
+@end
+
+@implementation JenkinsCrashTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
